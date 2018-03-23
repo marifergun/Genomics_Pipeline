d1e4ef92ab8d1985bf1b8b7aa804c77020ea2b0d
bioinformaticslab <32200853+bioinformaticslab@users.noreply.github.com>
2018-03-23 23:12:12 +0300

    gui integration

---
+++ pipeline_gui.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+import dna_pipeline as dna
+
+class PipelineGui:
+
+    def __init__(self):
+        self.parent = tk.Tk()
+        self.parent.geometry('650x600')
+        self.label0 = tk.Label(self.parent, text="Pipeline GUI", fg="black", font=("Times", 40, "bold italic")) \
+            .grid(column=3, row=0, sticky="SE", padx=10)
+
+        self.label1 = tk.Label(self.parent, text="1. Pipeline", fg="black", font=("Times", 30, "bold")) \
+            .grid(column=3, row=1, sticky="SE", padx=10)
+
+        self.label2 = tk.Label(self.parent, text="Working Directory: ", fg="black", font=("Times", 15)) \
+            .grid(column=0, row=2, columnspan=2)
+        self.working_directory = tk.Entry(self.parent)
+        self.working_directory.grid(column=2, row=2, columnspan=4)
+
+        self.label3 = tk.Label(self.parent, text="Mapping Type: ", fg="black", font=("Times", 15)) \
+            .grid(column=0, row=3, columnspan=2)
+
+        self.var_maptype = tk.StringVar()
+
+        self.map_type1 = tk.Radiobutton(self.parent, text="Bwa", variable=self.var_maptype, value="Bwa") \
+            .grid(column=2, row=3)
+        self.map_type2 = tk.Radiobutton(self.parent, text="Bowtie2", variable=self.var_maptype, value="Bowtie") \
+            .grid(column=3, row=3)
+
+        self.label4 = tk.Label(self.parent, text="Sample Type: ", fg="black", font=("Times", 15)) \
+            .grid(column=0, row=4, columnspan=2)
+
+        self.var_sampletype = tk.StringVar()
+
+        self.sample_type1 = tk.Radiobutton(self.parent, text="Tumor", variable=self.var_sampletype, value="Tumor") \
+            .grid(column=2, row=4)
+        self.sample_type2 = tk.Radiobutton(self.parent, text="Germline", variable=self.var_sampletype, value="Germline") \
+            .grid(column=3, row=4)
+
+        self.label5 = tk.Label(self.parent, text="Library ID: ", fg="black", font=("Times", 15)) \
+            .grid(column=0, row=5, columnspan=2)
+        self.library = tk.Entry(self.parent)
+        self.library.grid(column=2, row=5, columnspan=4)
+
+        self.label6 = tk.Label(self.parent, text="Threads: ", fg="black", font=("Times", 15)) \
+            .grid(column=0, row=6, columnspan=2)
+        self.threads = tk.Entry(self.parent)
+        self.threads.grid(column=2, row=6, columnspan=4)
+
+        self.submit_button = tk.Button(self.parent, text="Submit", command=self.OnClick, width=20).grid(row=7, column=2)
+        self.asd = "sadsadsa"
+        self.parent.mainloop()
+
+    def OnClick(self):
+        print(self.asd)
+        mt = self.var_maptype.get()
+        st = self.var_sampletype.get()
+        wd = self.working_directory.get()
+        lb = self.library.get()
+        th = self.threads.get()
+
+        pipeline1 = dna.BamPipeline(working_directory=wd, map_type=mt, sample_type=st, library_matching_id= lb, thrds=th)
+        pipeline1.run_pipeline()
+
+
+
+PipelineGui()
