15c7ed32de2007792c02dcc00d03f3d463e4fa38
bioinformaticslab <batuhankisakol@std.sehir.edu.tr>
2018-04-12 03:41:19 +0300

    deneme

---
+++ paths.py
@@ -0,0 +1,9 @@
+
+class GetPaths(object):
+    def __init__(self):
+        self.picard_path = "/media/data/AMBRY/picard.jar"
+        self.gatk_path = "/media/data/AMBRY/GenomeAnalysisTK.jar"
+        self.ref_dir = "/media/data/ref_genome_indexes/"
+        self.varscan_path = "/media/data/AMBRY/VarScan.v2.3.9.jar"
+        self.dbsnp = "/media/data/ref_genome_indexes/hg19_bundle/dbsnp_138.hg19.vcf"
+        self.cosmic = "/media/data/ref_genome_indexes/hg19_bundle/Mills_and_1000G_gold_standard.indels.hg19.vcf"
